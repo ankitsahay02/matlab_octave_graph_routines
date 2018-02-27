84fe49e1e056829f74159b0baab4204df7dce617
Keivan Hassani Monfared <k1monfared@users.noreply.github.com>
2018-02-27 15:44:00 -0700

    Create random_multi_bottleneck_simple_graph.m
    Inputs:
       N: a list of k numbers of vertices in each part, that adds up to n,
           total number of vertices.
           Default: N = [10, 15]
       P: a kxk symetric matrix with (i,j) entry being the the
           probability of and edge between part i and j.
           Default: identity matrix of size k, i.e. the output will have
           k disconnected components and each connected component is a
           complete graph.
    
     Outputs:
       B: an nxn adjacency matrix of the graph
    
     2-part Example :
     N = [15,25];
     P = [.90,.10; 
          .10,.85];
    
     3-part Example :
     N = [20,15,25];
     P = [.90,.10,.10; 
          .10,.85,.20; 
          .10,.20,.90];

---
+++ random_multi_bottleneck_simple_graph.m
@@ -0,0 +1,61 @@
+function B = random_multi_bottleneck_simple_graph(N,P)
+    % Inputs:
+    %   N: a list of k numbers of vertices in each part, that adds up to n,
+    %       total number of vertices.
+    %       Default: N = [10, 15]
+    %   P: a kxk symetric matrix with (i,j) entry being the the
+    %       probability of and edge between part i and j.
+    %       Default: identity matrix of size k, i.e. the output will have
+    %       k disconnected components and each connected component is a
+    %       complete graph.
+    %
+    % Outputs:
+    %   B: an nxn adjacency matrix of the graph
+    %
+    % 2-part Example :
+    % N = [15,25];
+    % P = [.90,.10; 
+    %      .10,.85];
+    %
+    % 3-part Example :
+    % N = [20,15,25];
+    % P = [.90,.10,.10; 
+    %      .10,.85,.20; 
+    %      .10,.20,.90];
+    
+    %taking care of the defaults
+    if nargin < 2
+        if nargin < 1
+            N = [10, 15];
+        end
+        P = eye(sum(N));
+    end
+    
+    n = sum(N); %total number of vertices
+    B = zeros(n); %initialize the adjacency matrix
+    
+    % add vertices 
+    p_adjust = 0; %for moving up each part in rows
+    k = 1;
+    while k <= length(N)
+        q_adjust = 0; %for moving up each part in columns
+        l = 1;
+        while l <= length(N)
+            p = P(k,l); %probability of edges between parts k and l
+            for r = p_adjust+1:p_adjust+N(k) %rows of  this part
+                for c = q_adjust+1:q_adjust+N(l) %columns of this part
+                    if (r>c) %only consider the lower triagnle
+                        if rand > 1-p %toss a coin
+                            B(r,c) = 1;
+                            B(c,r) = 1; %then symmetrize it
+                        end
+                    end
+                end
+            end
+            q_adjust = q_adjust + N(l); %move on to next part of columns
+            l = l+1;
+        end
+        p_adjust = p_adjust + N(k); %move on to next part of rows
+        k = k+1;
+    end
+end
